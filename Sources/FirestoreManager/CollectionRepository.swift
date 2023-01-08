3b4e842507b2d0944f278179f47959141dfce1e4
annanna <anna.muenster@web.de>
2023-01-08 22:38:28 +0100

    - Added CollectionRepository for standard functions (fetch, create, update, delete)

---
+++ Sources/FirestoreManager/CollectionRepository.swift
@@ -0,0 +1,69 @@
+//
+//  CollectionRepository.swift
+//  
+//
+//  Created by Anna Münster on 08.01.23.
+//
+
+import Combine
+import Foundation
+
+open class CollectionRepository<T: Codable>: ObservableObject {
+    
+    // MARK: - Variables
+    
+    public var objects = CurrentValueSubject<[T], Never>([])
+    public var filters: [String: Any]?
+    public var order: [String]?
+    public let ref: ReferenceProtocol
+    
+    public init(reference: ReferenceProtocol, filters: [String: Any]? = nil, order: [String]? = nil) {
+        self.ref = reference
+        self.filters = filters
+        self.order = order
+        fetchCollection(withListener: false)
+    }
+    
+    open func fetchCollection(withListener: Bool) {
+        FirestoreManager.fetchCollection(ref, filters: filters, orderBy: order, withListener: withListener) { (result: Result<[T], FirestoreError>) in
+            switch result {
+            case .success(let objects):
+                self.objects.send(objects)
+            case .failure(let error):
+                self.didReceiveError(error)
+            }
+        }
+    }
+    
+    open func create(_ object: T) {
+        FirestoreManager.createDocument(object, reference: ref) { result in
+            switch result {
+            case .success(let id):
+                print("Created object \(self.getPath(id: id))!")
+            case .failure(let error):
+                self.didReceiveError(error)
+            }
+        }
+    }
+    
+    private func getPath(id: String) -> String {
+        String(describing: try? ref.reference().path) + "/\(id)"
+    }
+    
+    open func update(_ object: T, id: String) {
+        FirestoreManager.updateDocument(object, id: id, reference: ref) { result in
+            switch result {
+            case .success:
+                print("Object updated \(self.getPath(id: id))!")
+            case .failure(let error):
+                self.didReceiveError(error)
+            }
+        }
+    }
+    
+    open func delete(id: String) {
+        FirestoreManager.deleteDocument(id: id, reference: ref)
+    }
+    
+    open func didReceiveError(_ error: Error) { }
+}
